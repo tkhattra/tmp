2df0f733ef8b6b09f4562cc16f69611d10519735
Taj Khattra <taj.khattra@gmail.com>
2022-04-22 17:44:19 -0700

    varint.py

---
+++ varint.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# varint encode/decode
+
+def enc(v):
+    l = []
+    while v >= 128:
+        l.append((v & 127) | 128)
+        v >>= 7
+    l.append(v)
+    return bytes(l)
+
+def dec(b):
+    v = 0
+    for i in range(len(b)):
+        v |= b[i] << 7*i
+    return v
